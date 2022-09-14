9c573557faa0d70c7f23e1313ee51b7015932ebc
xre190 <bppereira@gmail.com>
2022-09-13 23:10:40 -0300

    :package: js budget calculation

--- script.js
+++ script.js
@@ -4,3 +4,31 @@ hamburguer.addEventListener("click", function () {
     document.querySelector(".container").classList.toggle("show-menu");
 
 });
+
+document.querySelector("#qty").addEventListener("change", updatePrice)
+document.querySelector("#js").addEventListener("change", updatePrice)
+document.querySelector("#layout-yes").addEventListener("change", updatePrice)
+document.querySelector("#layout-no").addEventListener("change", updatePrice)
+document.querySelector("#deadline").addEventListener("change", function(){
+    const deadline = document.querySelector("#deadline").value
+    if (deadline == "1") {
+        document.querySelector("label[for=deadline]").innerHTML = "Prazo: " + deadline + " semana"
+    } else {
+        document.querySelector("label[for=deadline]").innerHTML = "Prazo: " + deadline + " semanas"
+    }
+    updatePrice()
+})
+
+function updatePrice(){
+    const qty = document.querySelector("#qty").value
+    const ifJS = document.querySelector("#js").checked
+    const layoutyes = document.querySelector("#layout-yes").checked
+    const deadline = document.querySelector("#deadline").value
+
+    let price = qty * 100
+    if (ifJS) price *= 1.1
+    if (layoutyes) price += 500
+    let urgency = 1 - deadline * 0.1
+    price *= 1 + urgency
+    document.querySelector("#price").innerHTML = "R$ " + price.toFixed(2)
+}
